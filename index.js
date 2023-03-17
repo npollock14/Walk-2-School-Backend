3dec4af8a59c48e8dc6f389f941ad668b892cb01
npollock14 <ndpollock@wpi.edu>
2023-03-17 13:54:35 -0400

    create account route

--- index.js
+++ index.js
@@ -25,6 +25,32 @@ async function authenticate(username, password) {
   return user;
 }
 
+async function createUser(username, password) {
+  const client = new MongoClient(MONGODB_URI);
+  await client.connect();
+  const db = client.db(DB_NAME);
+  const collection = db.collection(COLLECTION_NAME);
+
+  // make sure the username is unique
+  // try to find a user with the same username
+  const existingUser = await collection.findOne({ username });
+  if (existingUser) {
+    return null;
+  }
+
+  // create a new user document
+  const newUser = { username, password };
+
+  // insert the new user into the database
+  const result = await collection.insertOne(newUser);
+
+  // close the database connection
+  await client.close();
+
+  return result.insertedId;
+}
+
+// authenticate a user using the username and hashed password provided from the client
 app.post("/authenticate", async (req, res) => {
   const { username, password } = req.body;
 
@@ -38,6 +64,32 @@ app.post("/authenticate", async (req, res) => {
     res.status(200).json({ message: "Authenticated" });
   } else {
     res.status(401).json({ message: "Invalid credentials" });
+  }
+});
+
+// create a new user account using the username and hashed password provided from the client
+app.post("/create-account", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Missing username or password" });
+  }
+
+  // make sure that the username is greater than 3 characters and less than 20 and uses only letters and numbers
+  if (
+    username.length < 3 ||
+    username.length > 20 ||
+    !/^[a-zA-Z0-9]+$/.test(username)
+  ) {
+    return res.status(400).json({ message: "Invalid Username Format" });
+  }
+
+  const userId = await createUser(username, password);
+
+  if (userId) {
+    res.status(200).json({ message: "Account created" });
+  } else {
+    res.status(400).json({ message: "Username already exists" });
   }
 });
 

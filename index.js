53d1c286921563c1662d01ba2977b905280c06dc
npollock14 <ndpollock@wpi.edu>
2023-03-18 03:38:21 -0400

    fix

--- index.js
+++ index.js
@@ -19,10 +19,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const client = new MongoClient(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const client = new MongoClient(MONGODB_URI);
 
 let usersCollection;
 

6718aa1a0c13d5af26f00637ae2d95b1ba470fb2
npollock14 <ndpollock@wpi.edu>
2023-04-04 18:24:00 -0400

    static files fix

--- index.js
+++ index.js
@@ -25,7 +25,17 @@ app.use(cookieParser());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(__dirname));
+app.use(
+  express.static(__dirname, {
+    setHeaders: function (res, path, stat) {
+      if (path.endsWith(".js")) {
+        res.setHeader("Content-Type", "application/javascript");
+      } else if (path.endsWith(".css")) {
+        res.setHeader("Content-Type", "text/css");
+      }
+    },
+  })
+);
 
 let client;
 
